941736e511b839330f1ce17b45aa7da4323652e5
meghnavaidya <mvaidya@iastate.edu>
2019-03-26 20:35:57 -0500

    some random things

--- Project1/src/RBTree.java
+++ Project1/src/RBTree.java
@@ -1,4 +1,55 @@
+/**
+ * Team members:
+ * @author Smruthi Sandhanam, Meghna Vaidya, and Kamini Saldanha
+ * 
+ * RBTree class, maintains operations on RBTree.
+ */
 
 public class RBTree {
+	/**
+	 * RB Tree constructor. It initializes nil node as well.
+	 */
+	public RBTree() {
+		//TODO: Add code as needed.
+	}
+	
+	/**
+	 * Returns the root of teh tree.
+	 * @return
+	 */
+	public Node getRoot() {
+		//TODO: Modify it accordingly.
+		return null;
+	}
+	
+	/**
+	 * Returns reference for the nil node, for the rbTree.
+	 * @return
+	 */
+	public Node getNILNode() {
+		//TODO: Modify it accordingly.
+		return null;
+	}
+	
+	/**
+	 * Returns the number of internal nodes in the tree.
+	 * @return
+	 */
+	public int getSize() {
+		//TODO: Modify it accordingly.
+		return 0;
+	}
+	
+	
+	/**
+	 * Returns the height of the tree.
+	 * @return
+	 */
+	public int getHeight() {
+		//TODO: Modify it accordingly.
+		return 0;
+	}
+	
+	
 
 }
