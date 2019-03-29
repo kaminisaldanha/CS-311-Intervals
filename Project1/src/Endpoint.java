6b3e45816252db1dc4ddd8e9a97d2d23e99d669e
meghnavaidya <mvaidya@iastate.edu>
2019-03-28 22:43:37 -0500

    Did endpoint things

--- Project1/src/Endpoint.java
+++ Project1/src/Endpoint.java
@@ -5,13 +5,26 @@
 * 
 */
 
-
-public class Endpoint {
-	
-	//Returns the endpoint value. For example if the Endpoint object represents the left 
+//Returns the endpoint value. For example if the Endpoint object represents the left 
 	//endpoint of the interval [1, 3], this would return -1
-	public int getValue(){
-		return -1;
+public class Endpoint {
+	private int value;
+	private int p;
+	
+	public Endpoint(int value, int p)
+	{
+		this.value = value;
+		this.p = p;
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	public int getP()
+	{
+		return this.p;
 	}
 
 }
