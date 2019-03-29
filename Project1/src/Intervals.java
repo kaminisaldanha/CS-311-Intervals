75f1a22c87bd30870473b3d4d729d0fb26c59b64
Smruthi Sandhanam <smruthis@iastate.edu>
2019-03-28 23:11:41 -0500

    Methods added to Interval Class

--- Project1/src/Intervals.java
+++ Project1/src/Intervals.java
@@ -1,4 +1,26 @@
 
 public class Intervals {
 
+	public Intervals(){
+		
+	}
+	
+	public void intervalInsert(int a, int b){
+		
+	}
+	
+	public boolean intervalDelete(int intervalID){
+		
+		return false;
+	}
+	
+	public int findPOM(){
+		
+		
+		return -1;	
+	}
+	
+	public RBTree getRBTree(){
+		return null;
+	}
 }
