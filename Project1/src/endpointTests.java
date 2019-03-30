773ac9e9713ea1b3c40fe0aed1937f3536feb499
meghnavaidya <mvaidya@iastate.edu>
2019-03-30 16:48:19 -0500

    endpoint test

--- Project1/src/endpointTests.java
+++ Project1/src/endpointTests.java
@@ -6,8 +6,13 @@ public class endpointTests {
 
     @Test
     public void testGetValue() {
-        Endpoint endpoint = new Endpoint(4, 1);
+        Endpoint endpoint = new Endpoint(4, -1);
         assertEquals(4, endpoint.getValue());
     }
 
+    @Test
+    public void testGetP() {
+        Endpoint endpoint = new Endpoint(4, -1);
+        assertEquals(-1, endpoint.getP());
+    }
 }
