0b7c777f2d382337cfd7f8d67b8a6ceba4ca2d61
meghnavaidya <mvaidya@iastate.edu>
2019-03-31 18:20:01 -0500

    Revert "endpoint tests"
    This reverts commit 5079a6da1dd71cd94afb993015a5e5db2f2202f4.

--- Project1/src/endpointTests.java
+++ Project1/src/endpointTests.java
@@ -3,8 +3,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class endpointTests {
-	private RBTree rb;
-
 
     @Test
     public void testGetValue() {
@@ -17,24 +15,4 @@ public class endpointTests {
         Endpoint endpoint = new Endpoint(4, -1);
         assertEquals(-1, endpoint.getP());
     }
-    
-    @Test
-	public void testP() {
-		Intervals intervals = new Intervals();
-
-		intervals.intervalInsert(0, 4); 
-		intervals.intervalInsert(1, 6); 
-		intervals.intervalInsert(3, 9);
-		intervals.intervalInsert(7, 11);
-		rb = intervals.getRBTree();
-		assertEquals(-1, rb.getRoot().getP());
-		assertEquals(1, rb.getRoot().getLeft().getP());
-		assertEquals(1, rb.getRoot().getLeft().getLeft().getP());
-		assertEquals(1, rb.getRoot().getLeft().getRight().getP());
-		assertEquals(1, rb.getRoot().getRight().getP());
-		assertEquals(-1, rb.getRoot().getRight().getLeft().getP());
-		assertEquals(-1, rb.getRoot().getRight().getRight().getP());
-		assertEquals(-1, rb.getRoot().getRight().getRight().getRight().getP()); 
-	}
-
 }
