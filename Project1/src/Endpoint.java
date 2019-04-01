25a42b687f24d0dc1461430ca712778a431094ae
meghnavaidya <mvaidya@iastate.edu>
2019-03-31 21:47:27 -0500

    Finished javadoc

--- Project1/src/Endpoint.java
+++ Project1/src/Endpoint.java
@@ -5,23 +5,40 @@
 * 
 */
 
-//Returns the endpoint value. For example if the Endpoint object represents the left 
-	//endpoint of the interval [1, 3], this would return -1
 public class Endpoint {
+	/**
+	 * Value of endpoint
+	 */
 	private int value;
+	/**
+	 * P value of endpoint
+	 */
 	private int p;
 	
+	/**
+	 * Endpoint constructor, sets values for p and value
+	 * @param value
+	 * @param p
+	 */
 	public Endpoint(int value, int p)
 	{
 		this.value = value;
 		this.p = p;
 	}
 	
+	/**
+	 * Returns endpoint value
+	 * @return
+	 */
 	public int getValue()
 	{
 		return this.value;
 	}
 	
+	/**
+	 * Return p value
+	 * @return
+	 */
 	public int getP()
 	{
 		return this.p;
